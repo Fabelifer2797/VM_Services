517b6d4bbed06cca878c2f86cdc518958ddb97f7
isaacbmt <isaacbenavides20115@gmail.com>
2021-03-06 12:37:49 -0800

    [Add] comments

--- ImageServer/main.c
+++ ImageServer/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ulfius.h>
 #include <string.h>
-#include "histogramEqualization.h"
+//#include "histogramEqualization.h"
 
 struct Config {
     char puerto [256];
@@ -17,25 +17,31 @@ void readConf() {
     char line[256];
     char * token;
     char * seps = ":\n";
-    file = fopen("../config.conf", "r");
+    file = fopen("/etc/server/config.conf", "r");
     if (file == NULL) {
         perror("Error opening the file");
     }
+    // Read config files lines
     while (fgets(line, 256, file)) {
         char key [256], val [256];
+        // Skip if first char is a comment
         if (line[0] == '#') {
             continue;
         }
+        // Read token delimited by colon
         token = strtok(line, seps);
         for (int i = 0; token != NULL; i++) {
+            // Set value to key
             if (i == 0) {
                 strcpy(key, token);
             }
+            // Set value to val
             else if (i == 1) {
                 strcpy(val, token);
             }
             token = strtok(NULL, seps);
         }
+        // Set values to config struct
         if (strcmp(key, "Puerto") == 0) {
             strcpy(config.puerto, val);
         }
@@ -66,7 +72,7 @@ int callback_options(const struct _u_request * request, struct _u_response * res
 }
 
 int callback_post (const struct _u_request * request, struct _u_response * response, void * user_data) {
-    printf("ffofofo %s\n", "fer");
+    printf("Recibiendo mensaje...\n");
     json_error_t j_err;
     size_t index;
     json_t * json_request = ulfius_get_json_body_request(request, &j_err), * json_response = NULL, * value = NULL;
@@ -75,15 +81,18 @@ int callback_post (const struct _u_request * request, struct _u_response * respo
         printf("error: %s \n", j_err.source);
         printf("error: %i \n", j_err.line);
     }
+    // Read each json array element and download its respective image
     json_array_foreach(json_request, index, value) {
         const char * name = json_string_value(json_object_get(value, "name"));
         const char * link = json_string_value(json_object_get(value, "link"));
         const char * username = json_string_value(json_object_get(value, "username"));
         const char * sel_function = json_string_value(json_object_get(value, "selected_function"));
         char * command;
+        // Applies histogram function to the image
         if (strcmp(sel_function, "1") == 0) {
             command = msprintf("curl %s --output %s/%s", link, config.dirHistograma, name);
         }
+        // Applies color function to the image
         else if (strcmp(sel_function, "2") == 0) {
             command = msprintf("curl %s --output %s/%s", link, config.dirColores, name);
         }
@@ -93,9 +102,11 @@ int callback_post (const struct _u_request * request, struct _u_response * respo
         printf("comando: %s\n", command);
         system(command);
     }
+    // Create response json
     json_response = json_object();
     json_object_set_new(json_response, "message", json_string("Correcto"));
     ulfius_set_json_body_response(response, 200, json_response);
+    // Destroy object references
     json_decref(json_response);
     json_decref(json_request);
     return U_CALLBACK_CONTINUE;
@@ -107,9 +118,9 @@ int callback_post (const struct _u_request * request, struct _u_response * respo
 int main(void) {
     struct _u_instance instance;
     char * rest;
+    // Read config.conf and set values to config struct
     readConf();
-    printf("%s\n", config.dirHistograma);
-    printf("%s\n", config.dirColores);
+
     int port = (int) strtol(config.puerto, &rest, 10);
     // Initialize instance with the port number
     if (ulfius_init_instance(&instance, port, NULL, NULL) != U_OK) {
@@ -121,6 +132,7 @@ int main(void) {
     ulfius_add_endpoint_by_val(&instance, "OPTIONS", NULL, "*", 0, &callback_options, NULL);
     ulfius_add_endpoint_by_val(&instance, "POST", "/contraste", NULL, 0, &callback_post, NULL);
 
+    // Allow all origins
     u_map_put(instance.default_headers, "Access-Control-Allow-Origin", "*");
     u_map_put(instance.default_headers, "Access-Control-Allow-Credentials", "true");
     u_map_put(instance.default_headers, "Cache-Control", "no-store");
@@ -136,6 +148,7 @@ int main(void) {
     }
     printf("End framework\n");
 
+    // Stops the framework
     ulfius_stop_framework(&instance);
     ulfius_clean_instance(&instance);
 
